c77884182958ee2999365ae6c269be6365704980
Nivinebenjamin <3032073@aeres.nl>
2023-03-16 14:12:03 +0100

    Nivine website HGAV03

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,6 +7,42 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(leafletmap);
 
+let mijnGeojsonLaag = L.geoJSON().addTo(leafletmap);
+
+let woonplaatsen = ['amsterdam', 'leeuwarden', 'groningen']
+let woonplaatsNaam = woonplaatsen[1];
+
+fetch(`https://api.pdok.nl/bzk/locatieserver/search/v3_1/free?q=${woonplaatsNaam}&rows=10`)
+.then(response => response.json())
+.then(data => {
+    //pak het id nr van het eersye object wat terugkomt 
+    console.log(data.response.docs[0].id)
+    let id = data.response.docs[0].id
+
+    //vraag de data op en zet op de kaart 
+    tekenDataOpKaart(id)
+})
+//Aan de hand van een ID de geometrie ophalen en op de kaart zetten. En vliegen naar die locatie 
+
+function tekenDataOpKaart(woonplaatsId){
+    const mijnEersteAPIrequest= `https://api.pdok.nl/bzk/locatieserver/search/v3_1/lookup?id=${woonplaatsId}&wt=json&fl=*`
+
+    fetch(mijnEersteAPIrequest,{})
+    .then(response => response.json())
+    .then (data => { 
+        console.log(data)
+        console.log(data.response.docs[0].geometrie_ll)
+    let = geojsonFeature = Terraformer.wktToGeoJSON(data.response.docs[0].geometrie_ll)
+    mijnGeojsonLaag.addData(geojsonFeature);
+    let centerCoordinates = Terraformer.wktToGeoJSON (data.response.docs[0].centroide_ll)
+    console.log(centerCoordinates)
+    leafletmap.flyTo(centerCoordinates.coordinates.reverse())
+    })
+}
+
+
+//end leaflet kaart//
+
 //openlayers kaart//
 const openlayersMap= new ol.Map({
     target:'openlayers-map',
