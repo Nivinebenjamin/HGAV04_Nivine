72f68de0a28b4057eef8388fa1951be7646e7770
Nivinebenjamin <3032073@aeres.nl>
2023-04-13 22:47:50 +0200

    final_HGAV03_website

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -214,11 +214,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(adresmap);
 
-var polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
-]).addTo(adresmap);
+
 
 polygon.bindPopup("Guru travel office");
 
